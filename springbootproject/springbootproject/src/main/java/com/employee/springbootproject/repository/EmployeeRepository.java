60adff27197dff2b9cf86df48a0adc73b0f49910
Sneha-NucleusTeq <sneha.bhate@nucleusteq.com>
2023-09-12 15:11:07 +0530

    first commit

--- springbootproject/springbootproject/src/main/java/com/employee/springbootproject/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.employee.springbootproject.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.employee.springbootproject.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
